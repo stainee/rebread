823d6d4fcee0301c138871b8a768a7f7d69b61b4
수산나 <@DESKTOP-6HVBE10>
2022-11-01 13:43:04 +0900

    feat: storeList 출력

--- src/main/java/kr/or/store/controller/StoreController.java
+++ src/main/java/kr/or/store/controller/StoreController.java
@@ -2,11 +2,13 @@ package kr.or.store.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import kr.or.store.model.service.StoreService;
 import kr.or.store.model.vo.Store;
+import kr.or.store.model.vo.StorePageData;
 
 @Controller
 public class StoreController {
@@ -19,4 +21,17 @@ public class StoreController {
 	public void storeUpdate(Store s) {
 		int result = sservice.updateStoreDetail(s);
 	}
+	
+	@RequestMapping(value="/allStoreList.do")
+	public String allStoreList(int reqPage,Model model) {
+		StorePageData spd = sservice.selectStoreList(reqPage);
+		//System.out.println(spd);
+		model.addAttribute("list",spd.getList());
+		model.addAttribute("pageNavi",spd.getPageNavi());
+		model.addAttribute("reqPage",spd.getReqPage());
+		model.addAttribute("numPerPage", spd.getNumPerPage());
+		return "store/storeList";
+	}
+	
+	
 }
