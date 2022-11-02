1f59700dbcf819ed4bcef2ad949a4a4e52aad325
Kusuck <wh3521@naver.com>
2022-11-02 09:53:03 +0900

    chore: 주문페이지 디비 저장 구현

--- src/main/java/kr/or/order/model/service/OrderService.java
+++ src/main/java/kr/or/order/model/service/OrderService.java
@@ -5,9 +5,27 @@ import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Service;
 
 import kr.or.order.model.dao.OrderDao;
+import kr.or.order.model.vo.Order;
 
 @Service
 public class OrderService {
 	@Autowired
 	private OrderDao dao;
+
+	public int insertOrder(Order o) {
+		int result = dao.insertOrder(o);
+		return result;
+	}
+
+	public int searchOrderNo() {
+		int orderNo = dao.searchOrderNo();
+		return orderNo;
+	}
+
+	public int insertPaymentKey(Order o) {
+		int result = dao.insertPaymentKey(o);
+		return result;
+	}
+
+	
 }
